7ef82abce307a338448122b7d312234263c6d5b2
XES54 <112929514+XES54@users.noreply.github.com>
2023-06-12 09:45:46 +0200

    v0.2

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Finder',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 47, 1, 1)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Finder Home Page'),
     );
   }
 }
